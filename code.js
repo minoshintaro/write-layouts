0c40d94badf4918868c31505f2b96c3c0af2eec0
shintata <mino.shintaro@gmail.com>
2023-03-16 22:59:37 +0900

    add

--- code.js
+++ code.js
@@ -1,4 +1,8 @@
-let message = 'Write Layouts :-)';
+let message;
+let state = {
+  autoLayout: false,
+  resizing: false
+};
 
 const keys = {
   row: ['row', 'flex'],
@@ -20,44 +24,19 @@ const convertStringToArray = string => string.split(/\s/);
 const convertStringToNumber = string => Number(string.replace(/[^0-9]/g, ''));
 const getNodeList = target => target.findAllWithCriteria({ types: ['FRAME'] });
 
-function editSizeProps(frame) {
-  let props = {
-    width: frame.width,
-    height: frame.height,
-  };
-  let cue = false;
-
-  const nameList = convertStringToArray(frame.name);
-  nameList.forEach(item => {
-    if(item.match(keys.width)) {
-      props.width = convertStringToNumber(item);
-      cue = true;
-    }
-  });
-  nameList.forEach(item => {
-    if(item.match(keys.aspectRatio)) {
-      const ratio = item.split('x').map(Number);
-      props.height = Math.trunc(props.width * ratio[1] / ratio[0]);
-      cue = true;
-    }
-  });
-
-  if(cue) {
-    frame.resizeWithoutConstraints(props.width, props.height);
-  }
-}
-
 function editAutoLayoutProps(frame) {
   let props = {
-    flexDirection: 'NONE',
-    justifyContent: 'MIN',
+    flexDirection: frame.layoutMode,
+    justifyContent: frame.primaryAxisAlignItems,
+    aliginItems: frame.counterAxisAlignItems,
+    mainSizng: frame.primaryAxisSizingMode,
+    subSizing: frame.counterAxisSizingMode,
     gap: 0,
     paddingTop: 0,
     paddingBottom: 0,
     paddingLeft: 0,
     paddingRight: 0
   }
-  let cue = false;
 
   const nameList = convertStringToArray(frame.name);
   nameList.forEach(item => {
@@ -88,17 +67,17 @@ function editAutoLayoutProps(frame) {
     } else {
       if(item === keys.row[0] || item === keys.row[1]) {
         props.flexDirection = 'HORIZONTAL';
-        cue = 'x';
+        state.autoLayout = true;
       } else if(item === keys.column[0] || item === keys.column[1]) {
         props.flexDirection = 'VERTICAL';
-        cue = 'y';
+        state.autoLayout = true;
       } else if(item === keys.justification[0]) {
         props.justifyContent = 'SPACE_BETWEEN';
       }
     }
   });
 
-  if(cue) {
+  if(state.autoLayout) {
     frame.layoutMode = props.flexDirection;
     frame.primaryAxisAlignItems = props.justifyContent;
     frame.itemSpacing = props.gap;
@@ -107,18 +86,58 @@ function editAutoLayoutProps(frame) {
     frame.paddingLeft = props.paddingLeft;
     frame.paddingRight = props.paddingRight;
   }
+
+  console.log(props);
+}
+
+function editSizeProps(frame) {
+  let props = {
+    width: frame.width,
+    height: frame.height,
+  };
+
+  const nameList = convertStringToArray(frame.name);
+  nameList.forEach(item => {
+    if(item.match(keys.width)) {
+      props.width = convertStringToNumber(item);
+      state.resizing = true;
+    }
+  });
+  nameList.forEach(item => {
+    if(item.match(keys.aspectRatio)) {
+      const ratio = item.split('x').map(Number);
+      props.height = Math.trunc(props.width * ratio[1] / ratio[0]);
+      state.resizing = true;
+    }
+  });
+
+  if(state.resizing) {
+    frame.resizeWithoutConstraints(props.width, props.height);
+  }
+
+  console.log(props);
 }
 
 for(const targetLayer of figma.currentPage.selection) {
-  if(targetLayer.type === 'FRAME' || targetLayer.type === 'COMPONENT' || targetLayer.type === 'INSTANCE' || targetLayer.type === 'RECTANGLE' ) {
-    editSizeProps(targetLayer);
-  }
   if(targetLayer.type === 'FRAME' || targetLayer.type === 'COMPONENT') {
     editAutoLayoutProps(targetLayer);
     for(const node of getNodeList(targetLayer)) {
       editAutoLayoutProps(node);
     }
   }
+  if(targetLayer.type === 'FRAME' || targetLayer.type === 'COMPONENT' || targetLayer.type === 'INSTANCE' || targetLayer.type === 'RECTANGLE' ) {
+    editSizeProps(targetLayer);
+  }
+}
+
+if(state.autoLayout && state.resizing) {
+  message = 'Auto Layout and Resizing';
+} else if(state.autoLayout) {
+  message = 'Auto Layout';
+} else if(state.resizing) {
+  message = 'Resizing';
+} else {
+  message = 'No change :-)';
 }
 
 figma.closePlugin(message);
