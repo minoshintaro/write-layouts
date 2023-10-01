419aa69c8754c7913c5067e1158f6e44336f4c4b
shintata <mino.shintaro@gmail.com>
2023-10-01 12:39:43 +0900

    delete

--- _ignore/patterns.ts
+++
@@ -1,8 +0,0 @@
-export const patterns = new Map();
-patterns.set('frameNumber', /^Frame [0-9]{1,9}/);
-patterns.set('ratio', /^[0-9]{0,4}x[0-9]{0,4}$/);
-patterns.set('sizeName', /^[0-9]{0,4}x[0-9]{0,4}$|^(w|h)-[0-9]{0,4}$/);
-patterns.set('width', /^w-[0-9]{0,4}$/);
-patterns.set('height', /^h-[0-9]{0,4}$/);
-patterns.set('notPropName', /^(?!(row|col|g-auto)$|(g|p|px|py|pt|pb|pl|pr|w|h)-[0-9]{0,4}$|[0-9]{0,4}x[0-9]{0,4}$)/);
-patterns.set('notNumber', /[^0-9]/g);
